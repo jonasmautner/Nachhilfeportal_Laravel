458d6eecdd01b960f1215782cebe9b2de8c41def
jonas <s1910456019@students.fh-hagenberg.at>
2022-05-14 10:47:04 +0200

    Change date

--- database/seeders/MeetingdatesTableSeeder.php
+++ database/seeders/MeetingdatesTableSeeder.php
@@ -17,7 +17,7 @@ class MeetingdatesTableSeeder extends Seeder
         $date1->save();
 
         $date2 = new Meetingdate();
-        $date2->day = Carbon::createFromDate(2021,3,10);
+        $date2->day = Carbon::createFromDate(2021,3,11);
         $date2->from = Carbon::create(2021, 3, 11, 13, 0, 0);
         $date2->to = Carbon::create(2021, 3, 11, 13, 30, 0);
         $date2->save();
